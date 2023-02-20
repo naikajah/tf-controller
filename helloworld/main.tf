c21b045646058268736d8fea48829d6873dee117
Piyush Kumar <piyush.kumar@contino.io>
2023-02-20 09:26:41 +0000

    add

--- helloworld/main.tf
+++ helloworld/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.3.1"
+}
+
+variable "subject" {
+  type        = string
+  default     = "world"
+  description = "subject to hello"
+}
+
 output "instance_ips" {
-  value = "my output"
+  value = "Hello, ${var.subject}"
 }
